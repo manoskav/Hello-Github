33a470ea87b640234c11f27b909797d9802aaaca
Manos Kav <kavvousanos.em@gmail.com>
2016-09-18 15:11:39 +0300

    Test of git no2
    Adding main.c, mylib.c, mylib.h

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mylib.h"
+
+void main(){
+	
+	
+	while(1);
+	
+	
+	
+}
---
+++ mylib.c
@@ -0,0 +1,17 @@
+
+int myfun(int a, int b){
+		
+	return a+b;
+}
+
+
+float myfun(float a, float b){
+		
+	return a+b;
+}
+
+
+double myfun(double a, double b){
+		
+	return a+b;
+}
